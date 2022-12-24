8ff118d9def94935a7ebc0b88a70c368c8a66702
yaseru04 <yasirrizkii04@gmail.com>
2022-12-24 17:47:48 +0700

    BisKotaGetPassenger

---
+++ src/TugasMateri1E.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TugasMateri1E {
+    public static void main(String[] args) {
+        ArrayList <int[]> StopGo = new ArrayList<int[]>();
+        StopGo.add(new int[] {10,0});
+        StopGo.add(new int[] {3,5});
+        StopGo.add(new int[] {2,5});
+        System.out.println((StopGo));
+    }
+    public static int GetPassenger(ArrayList<int[]>Biskota) {
+        return 5;
+        }
+    }
